80024341908021a8426b9c9f8fbc4f83cbe9af65
Antoine Guillo <antoine.guillo2024@campus-eni.fr>
2024-11-28 10:08:55 +0100

    Affichage de la liste des souhaits et du détail d'un souhait

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,17 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class WishController extends AbstractController
 {
     #[Route('/list', name: 'wish_list', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(WishRepository $wishRepository): Response
     {
+
+        $wishes = $wishRepository->findAllRecentFirst();
+
         return $this->render('wish/list.html.twig', [
             'controller_name' => 'WishController',
+            'wishes' => $wishes,
         ]);
     }
 
-    #[Route('/detail/{id}', name: 'wish_detail', methods: ['GET', 'POST'])]
-    public function detail(int $id): Response {
+    #[Route('/detail/{id}', name: 'wish_detail', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function detail(Wish $wish): Response {
+
         return $this->render('wish/detail.html.twig', [
             'controller_name' => 'WishController',
+            'wish' => $wish,
         ]);
     }
 }
